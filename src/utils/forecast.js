121e0015e23567f697ad6d5d71917d7d31ca7e15
Ashutosh gupta <ashutosh.gupta.civ20@itbhu.ac.in>
2022-01-21 20:05:16 +0530

    change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -19,13 +19,9 @@ const forecast = (latitude,longitude, callback) => {
                             "Humidity - " + body.current.humidity + "\n" +
                             "Precipitation amount - " + body.current.precip_mm + "mm \n" +
                             "Cloud cover is " + body.current.cloud + "% \n" +
-                            //"Air Quality : " + body.current.air_quality + "\n" ;
                             "Last Updated : " + body.current.last_updated + "\n" ;
 
             callback(undefined,getback);
-                // localTime = response.body.location.LocalTime,
-                // weatherText = response.body.current.condition.text,
-                // icon = response.body.current.condition.icon,
         }
     });
 
@@ -36,25 +32,6 @@ module.exports = forecast;
 
 
 
-
-// const url = 'http://api.weatherapi.com/v1/current.json?key=58cf65c931a74c64aae141221212503&q=28.010952,73.314789'               
-
-// request({ url:url,json:true},(error, response)=>{
-    
-    
-// })
-//
-
-
-
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
 
 
 
--- src/utils/geocoding.js
+++ src/utils/geocoding.js
@@ -19,21 +19,3 @@ const geocode =(address,callback) => {
 }
 
 module.exports = geocode
-
-//geocoding
-
-//address ->lat/long -> weather
-
-// const url_geocode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/varanasi.json?access_token=pk.eyJ1IjoicnA3NyIsImEiOiJja21rZXc4NXMxMGRhMnduYXhzZ3NwMzYxIn0.anlNbCiooA0_Lk5Qrxc4Jw&limit=1'
-
-// request({ url:url_geocode,json:true},(error, response)=>{
-//     if(error){
-//         console.log("Unable to connect to the geocoding service! Try Again?");
-//     } else if(response.body.features.length===0){
-//         console.log("Please enter valid input (Enter a location name properly!)")
-//     } else{
-//         const latitude = response.body.features[0].center[1];
-//         const longitude = response.body.features[0].center[0];
-//         console.log(latitude,longitude);
-//     }
-// })
